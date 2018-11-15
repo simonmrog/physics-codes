deef8b5e4db7e9817cb19c2e76c77df40d46d8fe
Simon <34112219+simonmrog@users.noreply.github.com>
2018-11-15 17:18:32 -0500

    Add files via upload

--- Projectile.java
+++ Projectile.java
@@ -3,7 +3,8 @@ import java.text.DecimalFormat;
 public class Projectile {
 	
 	DecimalFormat format;
-	double g, initSpeed, angleDeg, angleRads, maxRange;
+	double g, initSpeed, angleDeg, angleRad;
+	double timeOfFlight;
 	
 	public Projectile(double v0, double angle) {
 		
@@ -11,19 +12,37 @@ public class Projectile {
 		g = 9.8;
 		initSpeed = v0;
 		angleDeg = angle;
-		angleRads = angleDeg * Math.PI/180; //conversion of the angle
+		angleRad = angleDeg * Math.PI/180; //conversion of the angle
 	}
 	
 	public void getMaximumRange() {
 		
 		//formula to calculate max range in a proyectile motion
-		maxRange = 2 * initSpeed * initSpeed * Math.sin(angleRads) * Math.cos(angleRads)
-				/ g;
+		double maxRange = 2 * initSpeed * initSpeed * Math.sin(angleRad) * 
+				Math.cos(angleRad) / g;
 		
 		//formatting output
 		String sRange = format.format(maxRange);
 		//printing result
-		System.out.println("Range of the projectile = " + sRange + " meters.");
+		System.out.println("Maximum range = " + sRange + " metres.");
+	}
+	
+	public void getTimeOfFlight() {
+		
+		double timeOfFlight = 2 * initSpeed * Math.sin(angleRad) / g;
+		
+		String sTime = format.format(timeOfFlight);
+		System.out.println("Time of flight = " + sTime + " seconds.");
+	}
+	
+	public void getMaximumHeight() {
+		
+		double maxHeight = initSpeed * initSpeed * Math.sin(angleRad) * Math.sin(angleRad)
+				/ 2 * g;
+		
+		String sHeight = format.format(maxHeight);
+		System.out.println("Maximum height = " + sHeight + " metres.");
+		
 	}
 	
 	public static void main(String[] args) {
@@ -31,8 +50,11 @@ public class Projectile {
 		//Use the next two lines if you want to grab the values directly by console
 		//double speed = Double.parseDouble(args[0]);
 		//double angleInDegrees = Double.parseDouble(args[1]);
-
-		Projectile P = new Projectile(12, 30);
+		
+		double angle = 30, speed = 3; //in degrees and m/s
+		Projectile P = new Projectile(speed, angle);
 		P.getMaximumRange();
+		P.getTimeOfFlight();
+		P.getMaximumHeight();
 	}
 }
